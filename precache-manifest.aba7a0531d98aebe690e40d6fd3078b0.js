39c0f37bc00d9b6d38d7d3b046fa42e5ba9c9040
John R. Estes <John.R.Estes@icloud.com>
2020-12-07 23:12:10 -0500

    Updates

---
+++ precache-manifest.aba7a0531d98aebe690e40d6fd3078b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "842fb0e84a7077eccb4f28a494debf33",
+    "url": "/musiclibraryreact/index.html"
+  },
+  {
+    "revision": "8b385d3810e189d0d722",
+    "url": "/musiclibraryreact/static/css/main.09b9a562.chunk.css"
+  },
+  {
+    "revision": "ea9a7f7fe090769af9f7",
+    "url": "/musiclibraryreact/static/js/2.08c7d356.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/musiclibraryreact/static/js/2.08c7d356.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b385d3810e189d0d722",
+    "url": "/musiclibraryreact/static/js/main.f8adad0b.chunk.js"
+  },
+  {
+    "revision": "a8845b14dd2183e24d66",
+    "url": "/musiclibraryreact/static/js/runtime-main.780cf0dd.js"
+  },
+  {
+    "revision": "ef61a99ba99b85ed49e6dd6d6e2f6359",
+    "url": "/musiclibraryreact/static/media/record.ef61a99b.jpg"
+  }
+]);
